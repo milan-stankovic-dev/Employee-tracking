f141fab079f2dd5ca67c16696957f20a08c60de5
magneticant <milance.za019@gmail.com>
2023-06-10 18:47:40 +0200

    Added controller classes for the entire domain

---
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/controller/TimeOffTypeController.java
@@ -0,0 +1,10 @@
+package rs.ac.bg.np.praksaprojekat.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/time-off-type")
+public class TimeOffTypeController {
+
+}
