bf17fea7cefd92fabe2e714ceaf91e096341a1d9
Braxon2 <dusanb80@gmail.com>
2023-06-10 20:27:16 +0200

    Added database connection to application.properties and we changed some mistakes

--- praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/repository/EmployeeTypeRepository.java
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/repository/EmployeeTypeRepository.java
@@ -2,7 +2,8 @@ package rs.ac.bg.np.praksaprojekat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import rs.ac.bg.np.praksaprojekat.domain.EmployeeType;
 
 @Repository
-public interface EmployeeTypeRepository extends JpaRepository <EmployeeTypeRepository,Long>{
+public interface EmployeeTypeRepository extends JpaRepository <EmployeeType,Long>{
 }
--- praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/repository/SingleEntryRepository.java
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/repository/SingleEntryRepository.java
@@ -2,7 +2,8 @@ package rs.ac.bg.np.praksaprojekat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import rs.ac.bg.np.praksaprojekat.domain.SingleEntry;
 
 @Repository
-public interface SingleEntryRepository extends JpaRepository<SingleEntryRepository,Long> {
+public interface SingleEntryRepository extends JpaRepository<SingleEntry,Long> {
 }
