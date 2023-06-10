0ecb99802b7ebe12e39d8bd135529166b68127ac
Braxon2 <dusanb80@gmail.com>
2023-06-10 18:57:11 +0200

    Changed from Integer to Long

--- praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/Repository/UserRepository.java
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/Repository/UserRepository.java
@@ -3,6 +3,6 @@ package rs.ac.bg.np.praksaprojekat.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rs.ac.bg.np.praksaprojekat.domain.User;
 
-public interface UserRepository extends JpaRepository<User,Integer>{
+public interface UserRepository extends JpaRepository<User,Long>{
 
 }
