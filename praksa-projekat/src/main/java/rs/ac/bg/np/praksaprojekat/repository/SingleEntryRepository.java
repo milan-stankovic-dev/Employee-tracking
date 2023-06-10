c373198047820ff8051be77679c44ffbc1a9f984
magneticant <milance.za019@gmail.com>
2023-06-10 19:29:04 +0200

    Changed package name from rs.ac.bg.np.praksaprojekat.Repository to rs.ac.bg.np.praksaprojekat.repository.

--- praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/repository/SingleEntryRepository.java
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/repository/SingleEntryRepository.java
@@ -1,4 +1,4 @@
-package rs.ac.bg.np.praksaprojekat.Repository;
+package rs.ac.bg.np.praksaprojekat.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
