dbceea91b33fadfbcbc01fbe4a1ba033f5f91084
Braxon2 <dusanb80@gmail.com>
2023-06-11 13:59:29 +0200

    JwtRequestFilter and JwtAuthenticationEntryPoint class has been made. And in class User we implemented UserDetails interface. loadByUsername method in UserService has been added.

--- praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/domain/User.java
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/domain/User.java
@@ -6,16 +6,21 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "app_user")
-public class User {
+public class User implements UserDetails {
 	//This atttribute would be used as username
 	private String email;
 	private String password;
+	private Collection<? extends GrantedAuthority> authorities;
 	
 	@Id
 	@GeneratedValue
@@ -24,4 +29,35 @@ public class User {
 	@JoinColumn(name = "employee_id")
 	@JsonIgnore
 	private Employee employee;
+
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.authorities;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
--- praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/service/UserService.java
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/service/UserService.java
@@ -1,4 +1,7 @@
 package rs.ac.bg.np.praksaprojekat.service;
 
+import rs.ac.bg.np.praksaprojekat.domain.User;
+
 public interface UserService {
+    User loadUserByUsername(String username);
 }
