6d1746bc26f55846c938598ec7e612f388ee1562
Braxon2 <dusanb80@gmail.com>
2023-06-14 20:31:04 +0200

    Added fields authenticationManager, userRepository, jwtService and method authenticate in class AuthenticationService, added class AuthenticationController and method called authenticate initself. In class AuthenticationResponse I added filed boolean admin.

---
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/auth/AuthenticationController.java
@@ -0,0 +1,24 @@
+package rs.ac.bg.np.praksaprojekat.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService service;
+
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+}
--- praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/auth/AuthenticationService.java
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/auth/AuthenticationService.java
@@ -1,7 +1,45 @@
 package rs.ac.bg.np.praksaprojekat.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import rs.ac.bg.np.praksaprojekat.config.JwtService;
+import rs.ac.bg.np.praksaprojekat.domain.Role;
+import rs.ac.bg.np.praksaprojekat.domain.User;
+import rs.ac.bg.np.praksaprojekat.repository.UserRepository;
+
 
 @Service
 public class AuthenticationService {
+
+    @Autowired
+    private  AuthenticationManager authenticationManager;
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private  JwtService jwtService;
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow();
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .isAdmin(user.getRole().equals(Role.ADMIN))
+                .build();
+    }
+
 }
