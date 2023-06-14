154735dee16683a77cf6224561103becc6ed2ca0
Mihajlo Pavlovic <pav.mihajlo@gmail.com>
2023-06-14 19:42:05 +0200

    Added SecurityConfiguration.java

---
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/config/SecurityConfiguration.java
@@ -0,0 +1,57 @@
+package rs.ac.bg.np.praksaprojekat.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+
+    private final AuthenticationProvider authenticationProvider;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+
+                .authorizeRequests()// authorizeHttpRequest
+                .requestMatchers("/api/v1/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+        //http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        // Ovako mora od spring security 6
+        http.sessionManagement((session)->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+        http .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+
+//
+//        http
+//                .csrf()
+//                .disable()
+//                .authorizeRequests()// authorizeHttpRequest
+//                .requestMatchers("")
+//                .permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//                .authenticationProvider(authenticationProvider)
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+//        return http.build();
+    }
+
+}
