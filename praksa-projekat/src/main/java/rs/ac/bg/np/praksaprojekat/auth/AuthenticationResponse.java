e979f4c045159479e51c70096e8228f00867e055
milan-stankovic-dev <milance.za019@gmail.com>
2023-06-14 20:06:53 +0200

    Added classes for Request and Response for Authentication in package auth.

---
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/auth/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package rs.ac.bg.np.praksaprojekat.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+	private String token;
+}
