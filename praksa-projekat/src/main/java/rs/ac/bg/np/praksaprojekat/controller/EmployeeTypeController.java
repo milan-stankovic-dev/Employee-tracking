ec99286a0c9214c8f4fc0db0709d8ea1204aef0c
Braxon2 <dusanb80@gmail.com>
2023-06-28 20:43:13 +0200

    Added logic for creating a new Type of employee.

--- praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/controller/EmployeeTypeController.java
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/controller/EmployeeTypeController.java
@@ -1,14 +1,25 @@
 package rs.ac.bg.np.praksaprojekat.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import rs.ac.bg.np.praksaprojekat.service.EmployeeTypeService;
+import rs.ac.bg.np.praksaprojekat.domain.EmployeeType;
+
+import rs.ac.bg.np.praksaprojekat.service.imp.EmployeeTypeServiceImp;
 
 @RestController
 @RequestMapping("/api/employee-type")
 public class EmployeeTypeController {
 
+
+
     @Autowired
-    private EmployeeTypeService employeeTypeService;
+    private EmployeeTypeServiceImp employeeTypeService;
+
+    @PostMapping("/newEmpType")
+    public EmployeeType createNewEmployeeType(String newEmpType) throws Exception {
+        return employeeTypeService.createNewEmployeeType(newEmpType);
+    }
+
 }
--- praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/service/imp/EmployeeTypeServiceImp.java
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/service/imp/EmployeeTypeServiceImp.java
@@ -1,15 +1,12 @@
 package rs.ac.bg.np.praksaprojekat.service.imp;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import rs.ac.bg.np.praksaprojekat.domain.Employee;
 import rs.ac.bg.np.praksaprojekat.domain.EmployeeType;
 import rs.ac.bg.np.praksaprojekat.repository.EmployeeTypeRepository;
 import rs.ac.bg.np.praksaprojekat.service.EmployeeTypeService;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeTypeServiceImp implements EmployeeTypeService {
@@ -17,14 +14,23 @@ public class EmployeeTypeServiceImp implements EmployeeTypeService {
     @Autowired
     private EmployeeTypeRepository employeeTypeRepository;
 
-    
-    public void init() {
-    	var employeeTypes = new ArrayList<EmployeeType>();
-    	employeeTypes.add(new EmployeeType(0L,"frontend", new HashSet<Employee>()));
-    	employeeTypes.add(new EmployeeType(0L,"backend", new HashSet<Employee>()));
-    	employeeTypes.add(new EmployeeType(0L,"management", new HashSet<Employee>()));
-    	employeeTypes.add(new EmployeeType(0L,"fullstack", new HashSet<Employee>()));
-    	
-    	employeeTypeRepository.saveAll(employeeTypes);
+
+    public EmployeeType createNewEmployeeType(String newEmpType) throws Exception {
+
+        Optional<EmployeeType> optionalEmployeeType = employeeTypeRepository.findByName(newEmpType);
+        if(optionalEmployeeType.isPresent()){
+            throw new Exception("This name of EmployeeType already exists!");
+        }
+
+
+        EmployeeType newTypeOfEmployee = new EmployeeType();
+        newTypeOfEmployee.setName(newEmpType);
+
+        employeeTypeRepository.save(newTypeOfEmployee);
+
+
+        return newTypeOfEmployee;
     }
+
+
 }
