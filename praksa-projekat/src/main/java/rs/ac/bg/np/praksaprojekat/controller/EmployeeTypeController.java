135305f017141a591efe1e87e0e81ba49078555b
milan-stankovic-dev <milance.za019@gmail.com>
2023-06-28 20:53:57 +0200

    Re-added init method in class EmployeeTypeServiceImpl and fixed param in EmployeeTypeController's endpoint for creating new employeeType.

--- praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/controller/EmployeeTypeController.java
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/controller/EmployeeTypeController.java
@@ -1,6 +1,7 @@
 package rs.ac.bg.np.praksaprojekat.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,9 @@ public class EmployeeTypeController {
     @Autowired
     private EmployeeTypeServiceImp employeeTypeService;
 
-    @PostMapping("/newEmpType")
-    public EmployeeType createNewEmployeeType(String newEmpType) throws Exception {
-        return employeeTypeService.createNewEmployeeType(newEmpType);
+    @PostMapping("/newEmpType/{type}")
+    public EmployeeType createNewEmployeeType(@PathVariable String type) throws Exception {
+        return employeeTypeService.createNewEmployeeType(type);
 
     }
 
--- praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/service/imp/EmployeeTypeServiceImp.java
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/service/imp/EmployeeTypeServiceImp.java
@@ -2,10 +2,14 @@ package rs.ac.bg.np.praksaprojekat.service.imp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import rs.ac.bg.np.praksaprojekat.domain.Employee;
 import rs.ac.bg.np.praksaprojekat.domain.EmployeeType;
 import rs.ac.bg.np.praksaprojekat.repository.EmployeeTypeRepository;
 import rs.ac.bg.np.praksaprojekat.service.EmployeeTypeService;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Optional;
 
 @Service
@@ -32,6 +36,15 @@ public class EmployeeTypeServiceImp implements EmployeeTypeService {
 
         return newTypeOfEmployee;
     }
-
+    
+    public void init() {
+    	var employeeTypes = new ArrayList<EmployeeType>();
+    	employeeTypes.add(new EmployeeType(0L,"frontend", new HashSet<Employee>()));
+    	employeeTypes.add(new EmployeeType(0L,"backend", new HashSet<Employee>()));
+    	employeeTypes.add(new EmployeeType(0L,"management", new HashSet<Employee>()));
+    	employeeTypes.add(new EmployeeType(0L,"fullstack", new HashSet<Employee>()));
+    	
+    	employeeTypeRepository.saveAll(employeeTypes);
+    }
 
 }
