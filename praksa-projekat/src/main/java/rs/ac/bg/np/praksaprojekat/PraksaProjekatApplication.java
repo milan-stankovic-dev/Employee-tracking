62b23054aa9e04c6558897af396339e9f6cf6f47
milan-stankovic-dev <milance.za019@gmail.com>
2023-06-28 20:39:09 +0200

    Added functionality for initializing EmployeeType-s.

--- praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/PraksaProjekatApplication.java
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/PraksaProjekatApplication.java
@@ -5,13 +5,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+
+import rs.ac.bg.np.praksaprojekat.service.EmployeeTypeService;
 import rs.ac.bg.np.praksaprojekat.service.UserService;
+import rs.ac.bg.np.praksaprojekat.service.imp.EmployeeTypeServiceImp;
 
 @SpringBootApplication
 public class PraksaProjekatApplication {
 
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private EmployeeTypeServiceImp employeeTypeServiceImp;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PraksaProjekatApplication.class, args);
 	}
@@ -19,5 +25,6 @@ public class PraksaProjekatApplication {
 	@EventListener(ApplicationReadyEvent.class)
 	public void setUp(){
 		userService.init();
+		employeeTypeServiceImp.init();
 	}
 }
--- praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/service/imp/EmployeeTypeServiceImp.java
+++ praksa-projekat/src/main/java/rs/ac/bg/np/praksaprojekat/service/imp/EmployeeTypeServiceImp.java
@@ -1,7 +1,13 @@
 package rs.ac.bg.np.praksaprojekat.service.imp;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import rs.ac.bg.np.praksaprojekat.domain.Employee;
+import rs.ac.bg.np.praksaprojekat.domain.EmployeeType;
 import rs.ac.bg.np.praksaprojekat.repository.EmployeeTypeRepository;
 import rs.ac.bg.np.praksaprojekat.service.EmployeeTypeService;
 
@@ -11,4 +17,14 @@ public class EmployeeTypeServiceImp implements EmployeeTypeService {
     @Autowired
     private EmployeeTypeRepository employeeTypeRepository;
 
+    
+    public void init() {
+    	var employeeTypes = new ArrayList<EmployeeType>();
+    	employeeTypes.add(new EmployeeType(0L,"frontend", new HashSet<Employee>()));
+    	employeeTypes.add(new EmployeeType(0L,"backend", new HashSet<Employee>()));
+    	employeeTypes.add(new EmployeeType(0L,"management", new HashSet<Employee>()));
+    	employeeTypes.add(new EmployeeType(0L,"fullstack", new HashSet<Employee>()));
+    	
+    	employeeTypeRepository.saveAll(employeeTypes);
+    }
 }
